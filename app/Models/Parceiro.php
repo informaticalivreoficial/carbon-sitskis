458d364de9fc75786c38c6233dc85bb158886bcd
renato-Aspire-E5-574 <remontanari@live.com>
2022-08-27 21:24:24 -0300

    patch update

--- app/Models/Parceiro.php
+++ app/Models/Parceiro.php
@@ -64,11 +64,11 @@ class Parceiro extends Model
             $cover = $images->first(['path']);
         }
 
-        if(empty($metaimg['path']) || !Storage::disk()->exists(env('AWS_PASTA') . $metaimg['path'])) {
-            if(empty($this->logomarca) || !Storage::disk()->exists(env('AWS_PASTA') . $this->logomarca)) {
+        if(empty($metaimg['path']) || !Storage::disk()->exists($metaimg['path'])) {
+            if(empty($this->logomarca) || !Storage::disk()->exists($this->logomarca)) {
                 return url(asset('backend/assets/images/image.jpg'));
             }
-            return Storage::url($this->logomarca, 300, 300);
+            return Storage::url($this->logomarca);
         }
 
         //return Storage::url(Cropper::thumb($metaimg['path'], 800, 418));
@@ -77,7 +77,7 @@ class Parceiro extends Model
 
     public function cover()
     {       
-        if(empty($this->logomarca) || !Storage::disk()->exists(env('AWS_PASTA') . $this->logomarca)) {
+        if(empty($this->logomarca) || !Storage::disk()->exists($this->logomarca)) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
@@ -87,7 +87,7 @@ class Parceiro extends Model
 
     public function nocover()
     {       
-        if(empty($this->logomarca) || !Storage::disk()->exists(env('AWS_PASTA') . $this->logomarca)) {
+        if(empty($this->logomarca) || !Storage::disk()->exists($this->logomarca)) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -93,7 +93,7 @@ class Post extends Model
             $cover = $images->first(['path']);
         }
 
-        if(empty($cover['path']) || !Storage::disk()->exists(env('AWS_PASTA') . $cover['path'])) {
+        if(empty($cover['path']) || !Storage::disk()->exists($cover['path'])) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
@@ -111,7 +111,7 @@ class Post extends Model
             $cover = $images->first(['path']);
         }
 
-        if(empty($cover['path']) ||!Storage::disk()->exists(env('AWS_PASTA') . $cover['path'])) {
+        if(empty($cover['path']) ||!Storage::disk()->exists($cover['path'])) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
